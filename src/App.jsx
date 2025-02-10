a7ad034239df84a08c686bfd2f80e4a56ceb6e71
ZeroNaur <chronodeia@gmail.com>
2025-02-10 22:27:36 +0700

    Add logics

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,37 @@ import { SearchSVG } from "./SearchSVG"
 function App() {
   const [ chosenZodiac, setChosenZodiac ] = useState(null);
   const [ chosenDate, setChosenDate ] = useState(null);
-  const [ article, setArticle ] = useState("Nothing has been searched yet.");
   
+  const [ searchedZodiac, setSearchedZodiac ] = useState(null);
+  const [ searchedDate, setSearchedDate ] = useState(null); 
+  const [ article, setArticle ] = useState("Nothing has been searched yet.");
+
+  const searchHoroscope = async (chosenZodiac, chosenDate) => {
+    if (!chosenZodiac) {
+      setArticle("Please choose a Zodiac to search, Naura.");
+      return;
+    }
+
+    if (!chosenDate) {
+      setArticle("Please choose a Date to search, Naura.")
+      return;
+    }
+    
+
+    try {
+      setSearchedZodiac("Loading...")
+      setArticle("Loading...")
+      const response = await fetch(`https://my-cors-proxy.chronodeia.workers.dev/?sign=${chosenZodiac}&date=${chosenDate}`);
+      const data = await response.json();
+      setSearchedZodiac(chosenZodiac);
+      setSearchedDate(data.data.date);
+      setArticle(data.data.horoscope_data);
+    }
+    catch (error) {
+      console.error("Error: " + error);
+    }
+  }
+
   return (
     <div id="main-container">
       <h1>Horoscope for Naura</h1>
@@ -50,7 +79,9 @@ function App() {
           }
           onChange={(e) => setChosenDate(e.target.value)}
         />
-        <button>
+        <button
+          onClick={() => searchHoroscope(chosenZodiac, chosenDate)}
+        >
           <SearchSVG />
         </button>
       </div>
@@ -58,15 +89,28 @@ function App() {
       <div id="content">
         <div className="top">
           <span className="label">
-            Naura's Horoscope for
+            {
+              searchedZodiac !== null ?
+                "Naura's horoscope for"
+              : "Search Result"
+            }
           </span>
           <span className="content-date">
-            Feb 10, 2025
+            {
+              searchedZodiac !== null ?
+                searchedDate !== null ?
+                  searchedDate
+                : "..."
+              : null
+            }
           </span>
         </div>
-        <article>
-          {article}
-        </article>
+        <div className="bottom overflow-y-support">
+          <span>{searchedZodiac}</span>
+          <article>
+            {article}
+          </article>
+        </div>
       </div>
     </div>
   )
